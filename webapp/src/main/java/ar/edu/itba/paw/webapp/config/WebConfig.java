131c40d70559274788461af9c91e967281cb7860
ichayer <ichayer@itba.edu.ar>
2023-04-03 14:07:50 -0300

    Add usage of @PropertySource annotation and Spring Environment

--- webapp/src/main/java/ar/edu/itba/paw/webapp/config/WebConfig.java
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/config/WebConfig.java
@@ -1,8 +1,11 @@
 package ar.edu.itba.paw.webapp.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 import org.springframework.jdbc.datasource.SimpleDriverDataSource;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -15,8 +18,12 @@ import javax.sql.DataSource;
 
 @EnableWebMvc
 @ComponentScan({"ar.edu.itba.paw.webapp.controller", "ar.edu.itba.paw.services", "ar.edu.itba.paw.persistence"})
+@PropertySource("classpath:application.properties")
 @Configuration
 public class WebConfig extends WebMvcConfigurerAdapter {
+
+    @Autowired
+    private Environment environment;
 
     @Bean
     public ViewResolver viewResolver() {
@@ -41,9 +48,9 @@ public class WebConfig extends WebMvcConfigurerAdapter {
         // Docker container is running on port 15432
         // We are using the postgres database and the postgres user
         // TODO: Check that this configuration does not affect the deployment. In case it does, ask how to mitigate it.
-        ds.setUrl("jdbc:postgresql://localhost:15432/postgres");
-        ds.setUsername("postgres");
-        ds.setPassword("postgres");
+        ds.setUrl(environment.getProperty("database.url"));
+        ds.setUsername(environment.getProperty("database.username"));
+        ds.setPassword(environment.getProperty("database.password"));
         return ds;
     }
 }
