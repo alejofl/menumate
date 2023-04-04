6ea6ac6768563fa67fe22727c38740a54ab2e274
ichayer <ichayer@itba.edu.ar>
2023-04-03 21:03:46 -0300

    Basic sending email

---
+++ services/src/main/java/ar/edu/itba/paw/services/EmailServiceImpl.java
@@ -0,0 +1,25 @@
+package ar.edu.itba.paw.services;
+
+import ar.edu.itba.paw.service.EmailService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+
+@Service
+public class EmailServiceImpl implements EmailService {
+
+    @Autowired
+    private JavaMailSender emailSender;
+
+    @Override
+    public void sendEmail(String to, String subject, String body) {
+
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setFrom("somebody@gmail.com");
+        message.setTo(to);
+        message.setText(body);
+        emailSender.send(message);
+    }
+}
