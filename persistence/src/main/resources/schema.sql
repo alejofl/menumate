b704731190e63d6a38a6edf1d0f2bd8750d67410
ichayer <ichayer@itba.edu.ar>
2023-04-10 23:48:01 -0300

    Add db tables
    Co-authored-by: Alejo Flores Lucey <aflores@itba.edu.ar>
    Co-authored-by: Nehuen Gabriel Llanos <nllanos@itba.edu.ar>
    Co-authored-by: Thomas Mizrahi <tmizrahi@itba.edu.ar>

--- persistence/src/main/resources/schema.sql
+++ persistence/src/main/resources/schema.sql
@@ -1,5 +1,77 @@
+CREATE TABLE IF NOT EXISTS images (
+    image_id SERIAL PRIMARY KEY,
+    data BYTEA NOT NULL,
+);
+
 CREATE TABLE IF NOT EXISTS users (
-    userId SERIAL PRIMARY KEY,
-    username VARCHAR(255) UNIQUE NOT NULL,
-    password VARCHAR(255) NOT NULL
+    user_id SERIAL PRIMARY KEY,
+    username VARCHAR(32) UNIQUE,
+    password VARCHAR(128),
+    email VARCHAR(256) UNIQUE NOT NULL,
+    image_id INT REFERENCES images(image_id) ON DELETE SET NULL,
+    is_active BOOLEAN NOT NULL DEFAULT FALSE,
+);
+
+CREATE TABLE IF NOT EXISTS restaurants (
+    restaurant_id SERIAL PRIMARY KEY,
+    name VARCHAR(32) NOT NULL,
+    logo_id INT REFERENCES images(image_id) ON DELETE SET NULL,
+    portrait_1_id INT REFERENCES images(image_id) ON DELETE SET NULL,
+    portrait_2_id INT REFERENCES images(image_id) ON DELETE SET NULL,
+    address TEXT,
+    description TEXT,
+    is_active BOOLEAN NOT NULL DEFAULT TRUE,
+);
+
+CREATE TABLE IF NOT EXISTS roles (
+    role_level INT PRIMARY KEY,
+    name VARCHAR(32) NOT NULL,
+);
+
+CREATE TABLE IF NOT EXISTS restaurant_roles (
+    user_id INT REFERENCES users(user_id) ON DELETE CASCADE NOT NULL,
+    restaurant_id INT REFERENCES restaurant(restaurant_id) ON DELETE CASCADE NOT NULL,
+    role_level INT REFERENCES roles(role_level) ON DELETE CASCADE NOT NULL,
+
+    PRIMARY KEY(user_id, restaurant_id),
+);
+
+CREATE TABLE IF NOT EXISTS categories (
+    category_id SERIAL PRIMARY KEY,
+    restaurant_id INT REFERENCES restaurant(restaurant_id) ON DELETE CASCADE NOT NULL,
+    name VARCHAR(32) NOT NULL,
+    order INT NOT NULL,
+
+    UNIQUE(restaurant_id, order),
+);
+
+CREATE TABLE IF NOT EXISTS products (
+    product_id SERIAL PRIMARY KEY,
+    category_id INT REFERENCES category(category_id) ON DELETE CASCADE NOT NULL,
+    name VARCHAR(32) NOT NULL,
+    price DECIMAL(10,2) NOT NULL,
+    description TEXT,
+    image_id INT REFERENCES images(image_id) ON DELETE SET NULL,
+);
+
+CREATE TABLE IF NOT EXISTS order_types (
+    order_type_id SERIAL PRIMARY KEY,
+    name VARCHAR(32) NOT NULL,
+);
+
+CREATE TABLE IF NOT EXISTS orders (
+    order_id SERIAL PRIMARY KEY,
+    order_type_id INT REFERENCES order_types(order_type_id) ON DELETE CASCADE NOT NULL,
+    restaurant_id INT REFERENCES restaurant(restaurant_id) ON DELETE CASCADE NOT NULL,
+    user_id INT REFERENCES users(user_id) ON DELETE CASCADE NOT NULL,
+    order_date TIMESTAMP NOT NULL,
+    table_number INT,
+    address TEXT,
+);
+
+CREATE TABLE IF NOT EXISTS products_x_order (
+    order_id INT REFERENCES order(order_id) ON DELETE CASCADE NOT NULL,
+    product_id INT REFERENCES product(product_id) ON DELETE CASCADE NOT NULL,
+
+    PRIMARY KEY(order_id, product_id),
 );
