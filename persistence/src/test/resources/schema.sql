7b965f519cb9e96f8fc62b663190a88884cf59d0
ThomasMiz <32400648+ThomasMiz@users.noreply.github.com>
2023-05-01 13:32:17 -0300

    Add string length validation to all fields in sql schema

--- persistence/src/test/resources/schema.sql
+++ persistence/src/test/resources/schema.sql
@@ -9,7 +9,7 @@ CREATE TABLE IF NOT EXISTS users
     user_id     SERIAL PRIMARY KEY,
     email       VARCHAR(320) UNIQUE NOT NULL,
     password    VARCHAR(60),
-    name        VARCHAR(48) NOT NULL,
+    name        VARCHAR(50) NOT NULL,
     date_joined TIMESTAMP NOT NULL DEFAULT now(),
     image_id    INT REFERENCES images (image_id) ON DELETE SET NULL,
     is_active   BOOLEAN NOT NULL DEFAULT FALSE
@@ -19,14 +19,14 @@ CREATE TABLE IF NOT EXISTS restaurants
 (
     restaurant_id SERIAL PRIMARY KEY,
     name          VARCHAR(50) NOT NULL,
-    email         VARCHAR(256) NOT NULL,
+    email         VARCHAR(320) NOT NULL,
     owner_user_id INT REFERENCES users (user_id) ON DELETE CASCADE NOT NULL,
     date_created  TIMESTAMP NOT NULL DEFAULT now(),
     logo_id       INT REFERENCES images (image_id) ON DELETE SET NULL,
     portrait_1_id INT REFERENCES images (image_id) ON DELETE SET NULL,
     portrait_2_id INT REFERENCES images (image_id) ON DELETE SET NULL,
-    address       TEXT,
-    description   TEXT,
+    address       VARCHAR(200),
+    description   VARCHAR(300),
     is_active     BOOLEAN NOT NULL DEFAULT TRUE
 );
 
@@ -43,7 +43,7 @@ CREATE TABLE IF NOT EXISTS categories
 (
     category_id   SERIAL PRIMARY KEY,
     restaurant_id INT REFERENCES restaurants (restaurant_id) ON DELETE CASCADE NOT NULL,
-    name          VARCHAR(32) NOT NULL,
+    name          VARCHAR(50) NOT NULL,
     order_num     SMALLINT NOT NULL,
 
     UNIQUE (restaurant_id, order_num)
@@ -53,9 +53,9 @@ CREATE TABLE IF NOT EXISTS products
 (
     product_id  SERIAL PRIMARY KEY,
     category_id INT REFERENCES categories (category_id) ON DELETE CASCADE NOT NULL,
-    name        VARCHAR(128) NOT NULL,
+    name        VARCHAR(150) NOT NULL,
     price       DECIMAL(10, 2) NOT NULL,
-    description TEXT,
+    description VARCHAR(300),
     image_id    INT REFERENCES images (image_id) ON DELETE SET NULL,
     available   BOOLEAN NOT NULL DEFAULT TRUE
 );
@@ -68,7 +68,7 @@ CREATE TABLE IF NOT EXISTS orders
     user_id        INT REFERENCES users (user_id) ON DELETE CASCADE NOT NULL,
     date_ordered   TIMESTAMP NOT NULL DEFAULT now(),
     date_delivered TIMESTAMP,
-    address        TEXT,
+    address        VARCHAR(300),
     table_number   SMALLINT
 );
 
@@ -78,7 +78,7 @@ CREATE TABLE IF NOT EXISTS order_items
     product_id  INT REFERENCES products (product_id) ON DELETE CASCADE NOT NULL,
     line_number SMALLINT NOT NULL,
     quantity    SMALLINT NOT NULL CHECK (quantity > 0),
-    comment     TEXT,
+    comment     VARCHAR(120),
 
     PRIMARY KEY (order_id, product_id, line_number)
 );
