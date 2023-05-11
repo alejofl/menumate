50f24d0fbc73b0d903b28876a48d09b9e73789de
ThomasMiz <32400648+ThomasMiz@users.noreply.github.com>
2023-05-10 21:52:10 -0300

    Fix typo on VerificationTokenJdbcDao

--- persistence/src/main/java/ar/edu/itba/paw/persistence/VerificationTokenJdbcDao.java
+++ persistence/src/main/java/ar/edu/itba/paw/persistence/VerificationTokenJdbcDao.java
@@ -10,7 +10,7 @@ import javax.sql.DataSource;
 @Repository
 public class VerificationTokenJdbcDao extends BaseTokenJdbcDao implements VerificationTokenDao {
 
-    private static final String TABLE_NAME = "user_resetpassword_codes";
+    private static final String TABLE_NAME = "user_verification_codes";
     @Autowired
     public VerificationTokenJdbcDao(final DataSource ds) {
         super(TABLE_NAME, ds);
