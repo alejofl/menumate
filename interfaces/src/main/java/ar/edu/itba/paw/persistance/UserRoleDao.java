7fb3091b8b7545ecde5c48830bc53f22edaaf3ca
ichayer <ichayer@itba.edu.ar>
2023-06-15 17:27:24 -0300

    Add UserRoleDao

---
+++ interfaces/src/main/java/ar/edu/itba/paw/persistance/UserRoleDao.java
@@ -0,0 +1,31 @@
+package ar.edu.itba.paw.persistance;
+
+import ar.edu.itba.paw.model.*;
+import ar.edu.itba.paw.util.PaginatedResult;
+
+import java.util.Optional;
+
+public interface UserRoleDao {
+
+    /**
+     * Gets a user's role or empty if said user has no roles.
+     */
+    Optional<UserRole> getRole(long userId);
+
+    /**
+     * Sets a user's role.
+     */
+    UserRole create(long userId, UserRoleLevel roleLevel);
+
+    /**
+     * Deletes user's role .
+     */
+    void delete(long userId);
+
+
+    /**
+     * Gets the users a given role.
+     */
+    PaginatedResult<User> getByRole(UserRoleLevel roleLevel, int pageNumber, int pageSize);
+
+}
