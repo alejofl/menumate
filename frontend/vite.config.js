8234583bcb72b2e1d0385be23d1bf3bfc7a472cb
Alejo Flores Lucey <alejo@misterflores.com>
2023-10-16 23:32:42 -0300

    Add linter and pre-commit hook

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,10 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ command, mode }) => {
-  return {
-    base: mode === "production" ? "/paw-2023a-01" : "/",
-    plugins: [react()],
-  }
-})
+export default defineConfig(({ mode }) => {
+    return {
+        base: mode === "production" ? "/paw-2023a-01" : "/",
+        plugins: [react()]
+    };
+});
