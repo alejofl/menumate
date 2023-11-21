9822a74abc21821946e60f6dca61926305982bef
Alejo Flores Lucey <alejo@misterflores.com>
2023-11-20 21:00:24 -0300

    Add hook to change page title

--- frontend/src/components/Page.jsx
+++ frontend/src/components/Page.jsx
@@ -1,7 +1,10 @@
 import Footer from "./Footer.jsx";
 import Navbar from "./Navbar.jsx";
+import { useTitle } from "../utils/useTitle.js";
 
-function Page({children}) {
+function Page({title, children}) {
+    useTitle(title);
+
     return (
         <>
             <Navbar/>
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -3,7 +3,7 @@ import Page from "../components/Page.jsx";
 function Home() {
     return (
         <>
-            <Page>
+            <Page title="Home">
                 <h1>Hola, probando</h1>
             </Page>
         </>
