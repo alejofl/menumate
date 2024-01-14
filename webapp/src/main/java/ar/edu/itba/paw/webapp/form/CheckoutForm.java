88249afd24070f35be5533592a04d26db08dd714
ichayer <ichayer@itba.edu.ar>
2024-01-14 18:29:04 -0300

    Delete unused forms and update form imports

--- webapp/src/main/java/ar/edu/itba/paw/webapp/form/CheckoutForm.java
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/form/CheckoutForm.java
@@ -14,8 +14,6 @@ import javax.validation.Valid;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
-import javax.ws.rs.DefaultValue;
-import javax.ws.rs.QueryParam;
 import java.util.ArrayList;
 import java.util.List;
 
