9390a291a7662cc030ff6bb33a2589a1ed95daae
ichayer <ichayer@itba.edu.ar>
2024-01-15 01:52:11 -0300

    Add custom media type in OrderController

--- webapp/src/main/java/ar/edu/itba/paw/webapp/api/CustomMediaType.java
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/api/CustomMediaType.java
@@ -6,6 +6,12 @@ public class CustomMediaType {
      * Image controller
      */
     public static final String APPLICATION_IMAGE = "application/vnd.menumate.images.v1+json";
+
+    /***
+     * Order controller
+     */
+    public static final String APPLICATION_ORDERS = "application/vnd.menumate.orders.v1+json";
+    public static final String APPLICATION_ORDER_ITEMS = "application/vnd.menumate.order.items.v1+json";
     public static final String USER_ROLE_V1 = "application/vnd.menumate.userRole.v1+json";
     public static final String APPLICATION_USER_RESETS_PASSWORD = "application/vnd.menumate.userResetsPassword.v1+json";;
     public static final String APPLICATION_LISTS_RESTAURANTS_REPORTS_COUNT = "application/vnd.menumate.restaurantWithUnhandledReports.v1+json";
