1fba42822c3dda90fc01531e23365edc15aa12a8
Alejo Flores Lucey <alejo@misterflores.com>
2024-01-24 22:48:12 -0300

    Replace restaurantsUrl and reviewsUrl for UriTemplates and replace harcodeed strings for uri template filling

--- frontend/src/contexts/ApiContext.jsx
+++ frontend/src/contexts/ApiContext.jsx
@@ -1,11 +1,14 @@
 import React, { useEffect, useState } from "react";
 import Api from "../data/Api.js";
+import UriTemplate from "uri-templates";
 
 const ApiContext = React.createContext({
     didDiscovery: false,
     ordersUrl: "",
     restaurantsUrl: "",
+    restaurantsUriTemplate: "",
     reviewsUrl: "",
+    reviewsUriTemplate: "",
     usersUrl: "",
     imagesUrl: ""
 });
@@ -14,17 +17,18 @@ export function ApiContextProvider({children}) {
     const [didDiscovery, setDidDiscovery] = useState(false);
     const [failedDiscovery, setFailedDiscovery] = useState(false);
     const [ordersUrl, setOrdersUrl] = useState("");
-    const [restaurantsUrl, setRestaurantsUrl] = useState("");
-    const [reviewsUrl, setReviewsUrl] = useState("");
+    const [restaurantsUriTemplateString, setRestaurantsUriTemplateString] = useState("");
+    const [reviewsUriTemplateString, setReviewsUriTemplateString] = useState("");
     const [usersUrl, setUsersUrl] = useState("");
     const [imagesUrl, setImagesUrl] = useState("");
 
     useEffect(() => {
         Api.get("/")
             .then(results => {
+                console.log(results);
                 setOrdersUrl(results.data.ordersUrl);
-                setRestaurantsUrl(results.data.restaurantsUrl);
-                setReviewsUrl(results.data.reviewsUrl);
+                setRestaurantsUriTemplateString(results.data.restaurantsUriTemplate);
+                setReviewsUriTemplateString(results.data.reviewsUriTemplate);
                 setUsersUrl(results.data.usersUrl);
                 setImagesUrl(results.data.imagesUrl);
             })
@@ -38,8 +42,10 @@ export function ApiContextProvider({children}) {
                 didDiscovery: didDiscovery,
                 failedDiscovery: failedDiscovery,
                 ordersUrl: ordersUrl,
-                restaurantsUrl: restaurantsUrl,
-                reviewsUrl: reviewsUrl,
+                restaurantsUriTemplate: UriTemplate(restaurantsUriTemplateString),
+                restaurantsUrl: UriTemplate(restaurantsUriTemplateString).fill({}),
+                reviewsUriTemplate: UriTemplate(reviewsUriTemplateString),
+                reviewsUrl: UriTemplate(reviewsUriTemplateString).fill({}),
                 usersUrl: usersUrl,
                 imagesUrl: imagesUrl
             }}>
