1e1328dcc749e2025dce870405923f28fe745be2
Alejo Flores Lucey <alejo@misterflores.com>
2024-01-26 22:55:27 -0300

    Update UserRoleForRestaurant to match API

--- frontend/src/data/model/UserRoleForRestaurant.js
+++ frontend/src/data/model/UserRoleForRestaurant.js
@@ -1,11 +1,14 @@
 import {ROLE_FOR_RESTAURANT} from "../../utils.js";
 
 export default class UserRoleForRestaurant {
-    constructor(restaurantUrl, role, selfUrl, userUrl) {
+    constructor(email, name, restaurantUrl, role, selfUrl, userUrl) {
+        this.email = email;
+        this.name = name;
         this.restaurantUrl = restaurantUrl;
         this.role = role;
         this.selfUrl = selfUrl;
         this.userUrl = userUrl;
+
         this.isOwner = role === ROLE_FOR_RESTAURANT.OWNER;
         this.isAdmin = role === ROLE_FOR_RESTAURANT.ADMIN || this.isOwner;
         this.isOrderHandler = role === ROLE_FOR_RESTAURANT.ORDER_HANDLER || this.isOwner || this.isAdmin;
@@ -14,6 +17,8 @@ export default class UserRoleForRestaurant {
     // Static method to create an instance from a JSON object
     static fromJSON(object) {
         return new UserRoleForRestaurant(
+            object.email,
+            object.name,
             object.restaurantUrl,
             object.role,
             object.selfUrl,
@@ -23,6 +28,8 @@ export default class UserRoleForRestaurant {
 
     static notEmployee() {
         return new UserRoleForRestaurant(
+            null,
+            null,
             null,
             null,
             null,
