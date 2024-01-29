7a7d7de4fb22b35c55a8082927fe4de3b4af58c2
ichayer <ichayer@itba.edu.ar>
2024-01-28 21:50:52 -0300

    Restore AccessDeniedExceptionMapper (sorry thomas)

---
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/mapper/AccessDeniedExceptionMapper.java
@@ -0,0 +1,24 @@
+package ar.edu.itba.paw.webapp.mapper;
+
+import ar.edu.itba.paw.webapp.utils.ControllerUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.access.AccessDeniedException;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.ExceptionMapper;
+import javax.ws.rs.ext.Provider;
+
+@Provider
+public class AccessDeniedExceptionMapper implements ExceptionMapper<AccessDeniedException> {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(AccessDeniedExceptionMapper.class);
+
+    @Override
+    public Response toResponse(AccessDeniedException exception) {
+        final Response.Status status = ControllerUtils.getCurrentUserIdOrNull() == null ? Response.Status.UNAUTHORIZED : Response.Status.FORBIDDEN;
+
+        LOGGER.error("AccessDeniedException: {} - Message: {} - Status code: {}", exception.getClass(), exception.getMessage(), status.getStatusCode());
+
+        return Response.status(status).entity(exception.getMessage()).build();
+    }
+}
