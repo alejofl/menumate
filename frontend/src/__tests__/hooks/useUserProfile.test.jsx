b4b31275d266e26308004a23410a4a39d25ba697
NehuenLlanosItba <nllanos@itba.edu.ar>
2024-01-30 17:39:43 -0300

    Addresses test done and passed.

--- frontend/src/__tests__/hooks/useUserProfile.test.jsx
+++ frontend/src/__tests__/hooks/useUserProfile.test.jsx
@@ -1,15 +1,55 @@
+/* eslint-disable no-magic-numbers */
 import {describe, it, expect} from "vitest";
 import {useUserService} from "../../hooks/services/useUserService.js";
 import Api from "../../data/Api.js";
 import {apiUrl} from "../setup/utils.js";
+import Address from "../../data/model/Address.js";
 
 describe("useUserService", () => {
     describe("registerAddress", () => {
         it("should return 201 Created", async () => {
             const userService = useUserService(Api);
             const response = await userService.registerAddress(apiUrl("/users/1/addresses"));
-            // eslint-disable-next-line no-magic-numbers
             expect(response.status).toBe(201);
+        });
+    });
+
+    describe("getAddresses", () => {
+        it("should return an array", async () => {
+            const userService = useUserService(Api);
+            const response = await userService.getAddresses(apiUrl("/users/1/addresses"));
+            expect(Array.isArray(response)).toBe(true);
+        });
+
+        it("should return an array of Address", async () => {
+            const userService = useUserService(Api);
+            const response = await userService.getAddresses(apiUrl("/users/1/addresses"));
+            response.forEach((item) => expect(item).toBeInstanceOf(Address));
+        });
+
+        it("should return correct data", async () => {
+            const userService = useUserService(Api);
+            const response = await userService.getAddresses(apiUrl("/users/1/addresses"));
+            expect(response[0].address).toEqual(expect.any(String));
+            expect(response[0].lastUsed).toEqual(expect.any(Date));
+            expect(response[0].name).toEqual(expect.any(String));
+            expect(response[0].selfUrl).toEqual(expect.any(String));
+        });
+    });
+
+    describe("updateAddress", () => {
+        it("should return 204 No Content", async () => {
+            const userService = useUserService(Api);
+            const response = await userService.updateAddress(apiUrl("/users/1/addresses/1"));
+            expect(response.status).toBe(204);
+        });
+    });
+
+    describe("deleteAddress", () => {
+        it("should return 204 No Content", async () => {
+            const userService = useUserService(Api);
+            const response = await userService.deleteAddress(apiUrl("/users/1/addresses/1"));
+            expect(response.status).toBe(204);
         });
     });
 });
--- frontend/src/__tests__/mocks/UserProfileMock.js
+++ frontend/src/__tests__/mocks/UserProfileMock.js
@@ -9,5 +9,38 @@ export const userHandler = [
         } else {
             return new HttpResponse(null, {status: 415});
         }
+    }),
+
+    http.get(apiUrl("/users/:id/addresses"), ({request}) => {
+        if (request.headers.get("Accept") === USER_ADDRESS_CONTENT_TYPE) {
+            return HttpResponse.json([
+                {
+                    "address": "Arenales 100",
+                    "lastUsed": "2024-01-30T16:59:49.591",
+                    "name": "Casa",
+                    "selfUrl": "URL"
+                },
+                {
+                    "address": "Bulnes 201",
+                    "lastUsed": "2024-02-30T16:59:49.591",
+                    "name": "Trabajo",
+                    "selfUrl": "URL"
+                }
+            ]);
+        } else {
+            return new HttpResponse(null, {status: 406});
+        }
+    }),
+
+    http.patch(apiUrl("/users/:id/addresses/:addressId"), ({request}) => {
+        if (request.headers.get("Content-Type") === USER_ADDRESS_CONTENT_TYPE) {
+            return new HttpResponse(null, {status: 204});
+        } else {
+            return new HttpResponse(null, {status: 415});
+        }
+    }),
+
+    http.delete(apiUrl("/users/:id/addresses/:addressId"), () => {
+        return new HttpResponse(null, {status: 204});
     })
 ];
