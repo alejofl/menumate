f65a2852a8e55242a6945799f23ba96e2c510690
Alejo Flores Lucey <alejo@misterflores.com>
2024-01-29 21:01:24 -0300

    Finish useOrderService tests

--- frontend/src/__tests__/hooks/useOrderService.test.jsx
+++ frontend/src/__tests__/hooks/useOrderService.test.jsx
@@ -1,15 +1,94 @@
-import { describe, it, expect } from "vitest";
+/* eslint-disable no-magic-numbers */
+import {describe, it, expect} from "vitest";
 import {useOrderService} from "../../hooks/services/useOrderService.js";
 import Order from "../../data/model/Order.js";
 import Api from "../../data/Api.js";
 import {apiUrl} from "../setup/utils.js";
+import OrderItem from "../../data/model/OrderItem.js";
+import PagedContent from "../../data/model/PagedContent.js";
 
 describe("useOrderService", () => {
+    describe("placeOrder", () => {
+        it("should return 201 Created", async () => {
+            const orderService = useOrderService(Api);
+            const response = await orderService.placeOrder(apiUrl("/orders"), {});
+            expect(response.status).toBe(201);
+        });
+    });
+
     describe("getOrder", () => {
-        it("should return an instance of Order class", async () => {
+        it("should return an instance of Order", async () => {
             const orderService = useOrderService(Api);
             const response = await orderService.getOrder(apiUrl("/orders/1"));
-            expect(response instanceof Order).toBe(true);
+            expect(response).toBeInstanceOf(Order);
+        });
+
+        it("should return correct data", async () => {
+            const orderService = useOrderService(Api);
+            const response = await orderService.getOrder(apiUrl("/orders/1"));
+            expect(response.orderId).toEqual(expect.any(Number));
+            expect(response.orderType).toEqual(expect.any(String));
+            expect(response.restaurantUrl).toEqual(expect.any(String));
+            expect(response.selfUrl).toEqual(expect.any(String));
+            expect(response.status).toEqual(expect.any(String));
+            expect(response.userUrl).toEqual(expect.any(String));
+            expect(response.itemsUrl).toEqual(expect.any(String));
+        });
+    });
+
+    describe("getOrderItems", () => {
+        it("should return an array", async () => {
+            const orderService = useOrderService(Api);
+            const response = await orderService.getOrderItems(apiUrl("/orders/1/items"));
+            expect(Array.isArray(response)).toBe(true);
+        });
+
+        it("should return an array of OrderItem", async () => {
+            const orderService = useOrderService(Api);
+            const response = await orderService.getOrderItems(apiUrl("/orders/1/items"));
+            response.forEach((item) => expect(item).toBeInstanceOf(OrderItem));
+        });
+
+        it("should return correct data", async () => {
+            const orderService = useOrderService(Api);
+            const response = await orderService.getOrderItems(apiUrl("/orders/1/items"));
+            expect(response[0].lineNumber).toEqual(expect.any(Number));
+            expect(response[0].productUrl).toEqual(expect.any(String));
+            expect(response[0].quantity).toEqual(expect.any(Number));
+        });
+    });
+
+    describe("getOrders", () => {
+        it("should return an instance of PagedContent", async () => {
+            const orderService = useOrderService(Api);
+            const response = await orderService.getOrders(apiUrl("/orders"));
+            expect(response).toBeInstanceOf(PagedContent);
+        });
+
+        it("should return an array of Order as content", async () => {
+            const orderService = useOrderService(Api);
+            const response = await orderService.getOrders(apiUrl("/orders"));
+            response.content.forEach((item) => expect(item).toBeInstanceOf(Order));
+        });
+
+        it("should return correct data", async () => {
+            const orderService = useOrderService(Api);
+            const response = await orderService.getOrders(apiUrl("/orders"));
+            expect(response.content[0].orderId).toEqual(expect.any(Number));
+            expect(response.content[0].orderType).toEqual(expect.any(String));
+            expect(response.content[0].restaurantUrl).toEqual(expect.any(String));
+            expect(response.content[0].selfUrl).toEqual(expect.any(String));
+            expect(response.content[0].status).toEqual(expect.any(String));
+            expect(response.content[0].userUrl).toEqual(expect.any(String));
+            expect(response.content[0].itemsUrl).toEqual(expect.any(String));
+        });
+    });
+
+    describe("updateStatus", () => {
+        it("should return 204 No Content", async () => {
+            const orderService = useOrderService(Api);
+            const response = await orderService.updateStatus(apiUrl("/orders/1"), {});
+            expect(response.status).toBe(204);
         });
     });
 });
--- frontend/src/__tests__/mocks/OrdersApiMock.js
+++ frontend/src/__tests__/mocks/OrdersApiMock.js
@@ -1,14 +1,22 @@
 import { http, HttpResponse } from "msw";
 import {apiUrl} from "../setup/utils.js";
-import {ORDERS_CONTENT_TYPE} from "../../utils.js";
+import {ORDER_ITEMS_CONTENT_TYPE, ORDERS_CONTENT_TYPE} from "../../utils.js";
 
 export const ordersHandlers = [
+    http.post(apiUrl("/orders"), ({request}) => {
+        if (request.headers.get("Content-Type") === ORDERS_CONTENT_TYPE) {
+            return new HttpResponse(null, {status: 201});
+        } else {
+            return new HttpResponse(null, {status: 415});
+        }
+    }),
+
     http.get(apiUrl("/orders/:id"), ({request, params}) => {
         if (request.headers.get("Accept") === ORDERS_CONTENT_TYPE) {
             return HttpResponse.json({
                 "dateOrdered": "2023-05-16T16:54:14.764599",
                 "itemsUrl": "URL",
-                "orderId": params.id,
+                "orderId": parseInt(params.id),
                 "orderType": "takeaway",
                 "restaurantUrl": "URL",
                 "selfUrl": "URL",
@@ -17,6 +25,96 @@ export const ordersHandlers = [
             });
         } else {
             return new HttpResponse(null, {status: 406});
+        }
+    }),
+
+    http.get(apiUrl("/orders/:id/items"), ({request}) => {
+        if (request.headers.get("Accept") === ORDER_ITEMS_CONTENT_TYPE) {
+            return HttpResponse.json([
+                {
+                    "lineNumber": 1,
+                    "productUrl": "URL",
+                    "quantity": 4
+                },
+                {
+                    "lineNumber": 2,
+                    "productUrl": "URL",
+                    "quantity": 1
+                }
+            ]);
+        } else {
+            return new HttpResponse(null, {status: 406});
+        }
+    }),
+
+    http.get(apiUrl("/orders"), ({request}) => {
+        if (request.headers.get("Accept") === ORDERS_CONTENT_TYPE) {
+            return HttpResponse.json([
+                {
+                    "dateConfirmed": "2023-05-13T13:38:44.213727",
+                    "dateDelivered": "2023-05-13T13:38:50.054667",
+                    "dateOrdered": "2023-05-13T13:38:28.798012",
+                    "dateReady": "2023-05-13T13:38:47.026651",
+                    "itemsUrl": "URL",
+                    "orderId": 1,
+                    "orderType": "takeaway",
+                    "restaurantUrl": "URL",
+                    "selfUrl": "URL",
+                    "status": "delivered",
+                    "userUrl": "URL"
+                },
+                {
+                    "dateConfirmed": "2023-05-23T18:35:27.058598",
+                    "dateDelivered": "2023-05-23T18:35:32.357684",
+                    "dateOrdered": "2023-05-23T18:34:58.691331",
+                    "dateReady": "2023-05-23T18:35:30.087099",
+                    "itemsUrl": "URL",
+                    "orderId": 7,
+                    "orderType": "takeaway",
+                    "restaurantUrl": "URL",
+                    "selfUrl": "URL",
+                    "status": "delivered",
+                    "userUrl": "URL"
+                },
+                {
+                    "address": "Edison 439, Martinez",
+                    "dateConfirmed": "2023-06-10T01:03:45.335",
+                    "dateDelivered": "2023-06-10T01:04:03.749",
+                    "dateOrdered": "2023-06-05T11:11:59.161896",
+                    "dateReady": "2023-06-10T01:04:00.736",
+                    "itemsUrl": "URL",
+                    "orderId": 10,
+                    "orderType": "delivery",
+                    "restaurantUrl": "URL",
+                    "selfUrl": "URL",
+                    "status": "delivered",
+                    "userUrl": "URL"
+                },
+                {
+                    "address": "---,Garay 415, Quilmes",
+                    "dateConfirmed": "2023-06-10T01:03:47.834",
+                    "dateDelivered": "2023-06-10T01:04:07.308",
+                    "dateOrdered": "2023-06-05T19:30:03.066291",
+                    "dateReady": "2023-06-10T01:03:55.512",
+                    "itemsUrl": "URL",
+                    "orderId": 12,
+                    "orderType": "delivery",
+                    "restaurantUrl": "URL",
+                    "selfUrl": "URL",
+                    "status": "delivered",
+                    "userUrl": "URL"
+                }
+            ]);
+        } else {
+            return new HttpResponse(null, {status: 406});
+        }
+    }),
+
+    http.patch(apiUrl("/orders/:id"), ({request}) => {
+        if (request.headers.get("Content-Type") === ORDERS_CONTENT_TYPE) {
+            return new HttpResponse(null, {status: 204});
+        } else {
+            return new HttpResponse(null, {status: 415});
         }
     })
 ];
