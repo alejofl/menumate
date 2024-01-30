dc5f72dfb6b0f49da9fc0a6e28efa7b10740dcf4
NehuenLlanosItba <nllanos@itba.edu.ar>
2024-01-30 18:45:38 -0300

    Test register, get and tokens done. Login missing.

--- frontend/src/__tests__/hooks/useUserProfile.test.jsx
+++ frontend/src/__tests__/hooks/useUserProfile.test.jsx
@@ -4,13 +4,119 @@ import {useUserService} from "../../hooks/services/useUserService.js";
 import Api from "../../data/Api.js";
 import {apiUrl} from "../setup/utils.js";
 import Address from "../../data/model/Address.js";
+import User from "../../data/model/User.js";
 
 describe("useUserService", () => {
+    describe("sendResetPasswordToken", () => {
+        it("should return 201 Created", async () => {
+            const userService = useUserService(Api);
+            const response = await userService.sendResetPasswordToken(apiUrl("/users/1"));
+            expect(response.status).toBe(201);
+        });
+    });
+
+    describe("resetPassword", () => {
+        it("should return 204 No Content", async () => {
+            const userService = useUserService(Api);
+            const response = await userService.resetPassword(apiUrl("/users/1"));
+            expect(response.status).toBe(204);
+        });
+    });
+
+    /*
+     * describe("login", () => {
+     *     it("should return an array", async () => {
+     *         const userService = useUserService(Api);
+     *         const response = await userService.getAddresses(apiUrl("/users/1/addresses"));
+     *         expect(Array.isArray(response)).toBe(true);
+     *     });
+     *
+     *     it("should return an array of Address", async () => {
+     *         const userService = useUserService(Api);
+     *         const response = await userService.getAddresses(apiUrl("/users/1/addresses"));
+     *         response.forEach((item) => expect(item).toBeInstanceOf(Address));
+     *     });
+     *
+     *     it("should return correct data", async () => {
+     *         const userService = useUserService(Api);
+     *         const response = await userService.getAddresses(apiUrl("/users/1/addresses"));
+     *         expect(response[0].address).toEqual(expect.any(String));
+     *         expect(response[0].lastUsed).toEqual(expect.any(Date));
+     *         expect(response[0].name).toEqual(expect.any(String));
+     *         expect(response[0].selfUrl).toEqual(expect.any(String));
+     *     });
+     * });
+     */
+
+    describe("register", () => {
+        it("should return 201 Created", async () => {
+            const userService = useUserService(Api);
+            const response = await userService.register(apiUrl("/users"));
+            expect(response.status).toBe(201);
+        });
+    });
+
     describe("registerAddress", () => {
         it("should return 201 Created", async () => {
             const userService = useUserService(Api);
             const response = await userService.registerAddress(apiUrl("/users/1/addresses"));
             expect(response.status).toBe(201);
+        });
+    });
+
+    describe("getUser", () => {
+        it("should return an object User", async () => {
+            const userService = useUserService(Api);
+            const response = await userService.getUser(apiUrl("/users/1"));
+            expect(response).toBeInstanceOf(User);
+        });
+
+        it("should return correct data", async () => {
+            const userService = useUserService(Api);
+            const response = await userService.getUser(apiUrl("/users/1"));
+            expect(response.addressesUrl).toEqual(expect.any(String));
+            expect(response.dateJoined).toEqual(expect.any(Date));
+            expect(response.email).toEqual(expect.any(String));
+            expect(response.isActive).toEqual(expect.any(Boolean));
+            expect(response.name).toEqual(expect.any(String));
+            expect(response.ordersUrl).toEqual(expect.any(String));
+            expect(response.preferredLanguage).toEqual(expect.any(String));
+            expect(response.restaurantsEmployedAtUrl).toEqual(expect.any(String));
+            expect(response.reviewsUrl).toEqual(expect.any(String));
+            expect(response.role).toEqual(expect.any(String));
+            expect(response.selfUrl).toEqual(expect.any(String));
+            expect(response.userId).toEqual(expect.any(Number));
+        });
+    });
+
+    describe("getUsers", () => {
+        it("should return an array", async () => {
+            const userService = useUserService(Api);
+            const response = await userService.getUsers(apiUrl("/users"));
+            expect(Array.isArray(response)).toBe(true);
+        });
+
+        it("should return an array of User", async () => {
+            const userService = useUserService(Api);
+            const response = await userService.getUsers(apiUrl("/users"));
+            response.forEach((item) => expect(item).toBeInstanceOf(User));
+        });
+
+        it("should return correct data", async () => {
+            const userService = useUserService(Api);
+            const response = await userService.getUsers(apiUrl("/users"));
+            expect(response[0].addressesUrl).toEqual(expect.any(String));
+            expect(response[0].dateJoined).toEqual(expect.any(Date));
+            expect(response[0].email).toEqual(expect.any(String));
+            expect(response[0].isActive).toEqual(expect.any(Boolean));
+            expect(response[0].name).toEqual(expect.any(String));
+            expect(response[0].ordersUrl).toEqual(expect.any(String));
+            expect(response[0].preferredLanguage).toEqual(expect.any(String));
+            expect(response[0].restaurantsEmployedAtUrl).toEqual(expect.any(String));
+            expect(response[0].reviewsUrl).toEqual(expect.any(String));
+            expect(response[0].role).toEqual(expect.any(String));
+            expect(response[0].selfUrl).toEqual(expect.any(String));
+            expect(response[0].userId).toEqual(expect.any(Number));
         });
     });
 
@@ -52,4 +158,13 @@ describe("useUserService", () => {
             expect(response.status).toBe(204);
         });
     });
+
+    /*
+     * describe("getRoleForRestaurant", () => {
+     *     it("should return a UserRoleForRestaurant", async () => {
+     *         const userService = useUserService(Api);
+     *         const response = await userService.getRoleForRestaurant(apiUrl("user/1/"))
+     *     })
+     * });
+     */
 });
