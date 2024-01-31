b081bf75d79a81b695b03245b045ca5b36a5bf50
ichayer <ichayer@itba.edu.ar>
2024-01-31 16:43:06 -0300

    Add logging in ValidationExceptionMapper

--- webapp/src/main/java/ar/edu/itba/paw/webapp/mapper/ValidationExceptionMapper.java
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/mapper/ValidationExceptionMapper.java
@@ -1,6 +1,8 @@
 package ar.edu.itba.paw.webapp.mapper;
 
 import ar.edu.itba.paw.webapp.dto.ValidationErrorDto;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.validation.ConstraintViolationException;
 import javax.ws.rs.core.GenericEntity;
@@ -11,9 +13,15 @@ import java.util.List;
 
 @Provider
 public class ValidationExceptionMapper implements ExceptionMapper<ConstraintViolationException> {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(ValidationExceptionMapper.class);
+
     @Override
     public Response toResponse(ConstraintViolationException exception) {
+        final Response.Status status = Response.Status.BAD_REQUEST;
+        LOGGER.error("ConstraintViolationException: {} - Message: {} - Status code: {}", exception.getClass().getName(), exception.getConstraintViolations(), status);
+
         final List<ValidationErrorDto> validationErrors = ValidationErrorDto.fromConstraintViolationCollection(exception.getConstraintViolations());
-        return Response.status(Response.Status.BAD_REQUEST).entity(new GenericEntity<List<ValidationErrorDto>>(validationErrors) {}).build();
+        return Response.status(status).entity(new GenericEntity<List<ValidationErrorDto>>(validationErrors) {}).build();
     }
 }
