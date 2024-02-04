a1149a31a8752278830f76ead248a81ec9fe01c7
Alejo Flores Lucey <alejo@misterflores.com>
2024-02-04 16:02:36 -0300

    Disable client-side cache handling, to honour server directives

--- webapp/src/main/java/ar/edu/itba/paw/webapp/config/WebAuthConfig.java
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/config/WebAuthConfig.java
@@ -135,6 +135,9 @@ public class WebAuthConfig extends WebSecurityConfigurerAdapter {
                     response.sendError(HttpServletResponse.SC_UNAUTHORIZED, ex.getMessage());
                 })
 
+                // Disable client-side cache handling
+                .and().headers().cacheControl().disable()
+
                 .and()
                 .addFilterBefore(jwtTokenFilter, UsernamePasswordAuthenticationFilter.class)
                 .addFilterBefore(authAnywhereFilter, UsernamePasswordAuthenticationFilter.class)
@@ -152,7 +155,6 @@ public class WebAuthConfig extends WebSecurityConfigurerAdapter {
     public CorsFilter corsFilter() {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(Collections.singletonList("*"));
         config.setAllowedHeaders(Collections.singletonList("*"));
         config.setAllowedMethods(Arrays.asList("GET", "HEAD", "POST", "OPTIONS", "DELETE", "PUT", "PATCH"));
         config.setExposedHeaders(Arrays.asList("X-MenuMate-AuthToken", "X-MenuMate-RefreshToken", "X-MenuMate-EmployeeUserCreated", "Content-Disposition", "Content-Language", "Location", "ETag", "Last-Modified", "Link", "WWW-Authenticate"));
